8c93e38feb6a663438ebbce0037e7f72c999825b
Jeff Gedney <jgedney@deloitte.com>
2021-01-08 20:59:57 -0500

    initial checkin

---
+++ xAPIReciever/Tester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String XAPIbody = "{\"id\":\"f3f9612e - 0e7e - 4d1d - 8047 - c4dcb2f01bb1\",\"actor\":{\"name\":\"Danny Devito\",\"mbox\":\"mailto: danny@example.org\"},\"verb\":{\"id\":\"http://adlnet.gov/expapi/verbs/completed\"},\"object\":{\"id\":\"https://example.org/block/1047831219\",\"definition\":{\"type\":\"https://w3id.org/xapi/cmi5/activities/block\"}},\"context\":{\"contextActivities\":{\"category\":[{\"id\":\"https://w3id.org/xapi/cmi5/context/categories/moveon\"}]},\"registration\":\"4a971479-af76-424c-a523-89a04476e7fd\"},\"timestamp\":\"2019-11-18T11:39:42.621Z\",\"result\":{\"completion\":true,\"duration\":\"PT13H33M26S\"}}";
+            dynamic msg = JsonConvert.DeserializeObject<dynamic>(XAPIbody);
+            Console.WriteLine(msg.GetType());
+            Console.WriteLine(msg.id);
+            Console.WriteLine(msg.actor.name);
+            Console.WriteLine(msg.actor.mbox);
+            Console.WriteLine(msg.verb.id);
+            Console.WriteLine(msg["object"].id);
+            Console.WriteLine(msg.context.contextActivities.category[0].id);
+            Console.WriteLine(msg.timestamp);
+        }
+    }
+}
---
+++ xAPIReciever/xAPIReceiver/Controllers/xAPIController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+
+
+namespace xAPIReceiver.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class xAPIController : ControllerBase
+    {
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+        }
+        
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        public HttpResponseMessage Post()
+        {
+            if (Request.IsHttps)
+            {
+                return Parsemessage(Request.Body, Request.Headers);
+            }
+            else
+            {
+                return new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+            }
+
+        }
+
+        private HttpResponseMessage Parsemessage(Stream body, IHeaderDictionary headers)
+        {
+            
+            HttpResponseMessage rsp = new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+            StreamReader sr = new StreamReader(body);
+            string message = sr.ReadToEnd();
+            dynamic msg = JsonConvert.DeserializeObject<dynamic>(message);
+            string output = String.Format("Actor: {0}, email: {1}, Verb: {2}, Category: {3}, Timestamp {4}", 
+                                  msg.actor.name,msg.actor.mbox,msg.verb.id,msg.context.contextActivities.category[0].id,msg.timestamp);            
+
+            return rsp;
+        }
+    }
+}
